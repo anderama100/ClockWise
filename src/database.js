6aab81dbd55600e015320085370af08814c8033d
anderson amaya <anderama100@gmail.com>
2020-04-13 13:40:58 -0600

    DBConnFixed

--- src/database.js
+++ src/database.js
@@ -1,8 +1,7 @@
 const mongoose=require('mongoose');
-// URL de conexion a la base de datos de Mongo en CloudMongo
-// usuario en Atlas andres.rivera@nvalue.com.co clave Soporte123
+
 const urlDB="mongodb+srv://anderama100:LJXXuxzR65MM7Gcz@clustercw-5mabe.mongodb.net/test?retryWrites=true&w=majority";
-// Establecer conexion con Mongo en Cloud
-mongoose.connect(urlDB).then(db=> console.log('Base de Datos Mongo Conectada en MongoAtlas')).catch(err=>console.log(err));
-// Exportar configuracion
+
+mongoose.connect(urlDB).then(db=> console.log('DataBase Connected on MongoAtlas')).catch(err=>console.log(err));
+
 module.exports=mongoose;
