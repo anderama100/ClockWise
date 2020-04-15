fb79297b1a80db9cbac4710bbae1ae2c6557c5ba
anderson amaya <anderama100@gmail.com>
2020-04-15 11:54:03 -0600

    Deploy

--- src/database.js
+++ src/database.js
@@ -1,7 +1,11 @@
 const mongoose=require('mongoose');
 
-const urlDB="mongodb+srv://anderama100:LJXXuxzR65MM7Gcz@clustercw-5mabe.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(process.env.urlDB, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
-mongoose.connect(urlDB).then(db=> console.log('DataBase Connected on MongoAtlas')).catch(err=>console.log(err));
+.then(db=> console.log('DataBase Connected on MongoAtlas')).catch(err=>console.log(err));
 
 module.exports=mongoose;
